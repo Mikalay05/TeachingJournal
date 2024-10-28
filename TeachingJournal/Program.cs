90af428b0d59ba3fca6a60ada80293fc05149f40
Rubick1337 <poss1337@gmail.com.>
2024-10-28 16:29:02 +0300

    test

--- TeachingJournal/Program.cs
+++ TeachingJournal/Program.cs
@@ -18,6 +18,7 @@ namespace TeachingJournal
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            //леша крутой
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
